cada546c60ebc4f747e842e6a32ddd65d911d95e
Evan <eroberts1998@icloud.com>
2023-06-17 20:25:31 -0400

    mostly finished scraper

--- scraper.py
+++ scraper.py
@@ -1,29 +1,114 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-from server.models import db, Set, Card
+import urllib.parse
 import ipdb
 import base64
 
 class Scraper:
-
-    def __init__(self):
+    def __init__(self, chrome_driver_path, base_url):
+        self.chrome_driver_path = chrome_driver_path
+        self.base_url = base_url
         self.cards = []
 
     def get_page(self, url):
-        doc = BeautifulSoup(requests.get(url).text, 'html.parser')
-        return doc
-    
+        options = Options()
+        options.add_argument("--headless")
+        service = Service(self.chrome_driver_path)
+        driver = webdriver.Chrome(service=service, options=options)
+        driver.get(url)
+        page_source = driver.page_source
+        driver.quit()
+        return page_source
+
     def get_cards(self, urls):
         for url in urls:
-            card_html = self.get_page(url)
-            
-            name = card_html.select('div.w-full.flex-1.px-5 h1.mt-4.mb-3')[0].text.strip()
-            image_element = card_html.select('div.relative.aspect-\\[\\.733\\] span img')[0]
-            image_url = image_element['src']
-            base64_image_data = image_url.split(',')[1]  # Extract the Base64-encoded data
+            try:
+                card_html = self.get_page(url)
+                soup = BeautifulSoup(card_html, 'html.parser')
 
-            
-            ipdb.set_trace()
+                # Image URL
+                try:
+                    image_element = soup.select('div.relative.aspect-[.733] span img')[0]
+                    image_url = image_element['src']
+                    image_url = urllib.parse.unquote(image_url).replace("amp;", "")
+                    full_image_url = f'{self.base_url}{image_url}'
+                except IndexError:
+                    full_image_url = None
+
+                # Title
+                try:
+                    title = soup.find('h1').text.strip()
+                except AttributeError:
+                    title = None
+
+                # Description
+                try:
+                    description = soup.find('dd', class_='load-external-scripts image-post_description').p.text.strip()
+                except AttributeError:
+                    description = None
+
+                # Element(s)
+                try:
+                    element_title = soup.find('dt', string='Element(s):').find_next('img')['title']
+                except AttributeError:
+                    element_title = None
+
+                 # Subclass(es)
+                subclass_titles = []
+                try:
+                    subclass_container = soup.find('dt', string='Subclass(es):').find_next('dd')
+                    subclass_imgs = subclass_container.find_all('img')
+                    if subclass_imgs:
+                        subclass_titles.append(subclass_imgs[0]['title'].strip())
+                    if len(subclass_imgs) > 1:
+                        subclass_titles.append(subclass_imgs[1]['title'].strip())
+                except AttributeError:
+                    pass
+
+                # Attack / Defense
+                try:
+                    attack_defense = soup.find('dt', string='Attack / Defense:').find_next('dd').text.strip()
+                except AttributeError:
+                    attack_defense = None
+
+                # Set Name
+                try:
+                    set_name = soup.find('dt', string='Set Name:').find_next('a').text.strip()
+                except AttributeError:
+                    set_name = None
+
+                # Set #
+                try:
+                    set_number = soup.find('dt', string='Set #:').find_next('dd').text.strip()
+                except AttributeError:
+                    set_number = None
+
+                # Rarity
+                try:
+                    rarity = soup.find('dt', string='Rarity:').find_next('dd').text.strip()
+                except AttributeError:
+                    rarity = None
+
+                # Rune Type
+                try:
+                    rune_type_element = soup.find('dt', string='Rune Type:')
+                    if rune_type_element:
+                        rune_type_img = rune_type_element.find_next('img')
+                        rune_type = rune_type_img['title'].strip()
+                    else:
+                        rune_type = None
+                except AttributeError:
+                    rune_type = None
+
+                # ...
+                ipdb.set_trace()
+            except Exception as e:
+                print(f"An error occurred while processing URL: {url}\nError: {str(e)}")
+                continue
+
+        return self.cards
 
 artist_collection_urls = [
     'https://www.elestrals.com/image/image__817a413f-a1dc-4e5a-8440-d11e59e492fb',
@@ -53,5 +138,5 @@ artist_collection_urls = [
     'https://www.elestrals.com/image/image__8ed18ec9-c893-4a6e-b9d1-9a6b22f3d1ab'
 ]
 
-scraper = Scraper()
+scraper = Scraper("drivers/chromedriver", "https://www.elestrals.com")
 artist_collection_list = scraper.get_cards(artist_collection_urls)
