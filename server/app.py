3c10ac30a57258b538d76e1f97fa79db7035e1d2
Evan <eroberts1998@icloud.com>
2023-06-18 21:42:06 -0400

    API routes established and working

--- server/app.py
+++ server/app.py
@@ -1,19 +1,89 @@
-from flask import Flask, jsonify, request, make_response
-from flask_cors import CORS
+#!/usr/bin/env python3
+
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+# DATABASE = os.environ.get(
+#     "DB_URI", f"sqlite://{os.path.join(BASE_DIR, 'instance/card_tracker.db')}"
+# )
+
+from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 from models import db, Set, Card
+import json
+import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///card_tracker.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    BASE_DIR, "instance/card_tracker.db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
 
-CORS(app)
-db.init_app(app)
 migrate = Migrate(app, db)
 
+db.init_app(app)
+
 api = Api(app)
+
+class Sets(Resource):
+
+    def get(self):
+        try:
+            sets_data = [set.to_dict(only=('id', 'name', 'icon_url', 'description')) for set in Set.query.all()]
+            response_data = json.dumps(sets_data)
+            return make_response(response_data, 200)
+        except:
+            return {"error": "404 Sets not found"}, 404
+
+api.add_resource(Sets, "/sets")
+
+class SetById(Resource):
+    def get(self, id):
+        try:
+            set = Set.query.filter_by(id=id).first()
+            if set:
+                set_data = set.to_dict()
+                response_data = json.dumps(set_data)  # Serialize OrderedDict to JSON
+                return make_response(response_data, 200)
+            else:
+                return make_response({"error": "404 Set not found"}, 404)
+        except:
+            return make_response({"error": "500 Internal Server Error"}, 500)
+
+
+api.add_resource(SetById, "/sets/<int:id>")
+
+class Cards(Resource):
+
+    def get(self):
+        try:
+            cards_data = [card.to_dict() for card in Card.query.all()]
+            response_data = json.dumps(cards_data)
+            return make_response(response_data, 200)
+        except:
+            return {"error": "404 Cards not found"}, 404
+
+api.add_resource(Cards, "/cards")
+
+class CardsById(Resource):
+
+    def get(self, id):
+        try:
+            card = Card.query.filter_by(id=id).first()
+            if card:
+                card_data = card.to_dict()
+                response_data = json.dumps(card_data)
+                return make_response(response_data, 200)
+            else:
+                return make_response({"error": "404 card not found"}, 404)
+        except:
+            return make_response({"error": "500 Internal Server Error"}, 500)
+
+api.add_resource(CardsById, '/cards/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,7 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+from collections import OrderedDict
 
-db = SQLAlchemy()
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(metadata=metadata)
 
 class Set(db.Model, SerializerMixin):
     __tablename__ = 'sets'
@@ -10,10 +25,33 @@ class Set(db.Model, SerializerMixin):
     name = db.Column(db.String(), nullable=False)
     icon_url = db.Column(db.String())
     description = db.Column(db.String())
-    cards = db.relationship('Card', backref='set', lazy=True)
+    cards = db.relationship('Card', back_populates='set')
 
     def __repr__(self):
         return f'<Set {self.name}>'
+    
+    def to_dict(self, only=None):
+        if only:
+            data = OrderedDict([
+                ('id', self.id),
+                ('name', self.name),
+                ('description', self.description),
+                ('icon_url', self.icon_url)
+            ])
+            for field in only:
+                if field == 'cards':
+                    data[field] = [card.to_dict() for card in self.cards]
+                elif hasattr(self, field):
+                    data[field] = getattr(self, field)
+        else:
+            data = OrderedDict([
+                ('id', self.id),
+                ('name', self.name),
+                ('description', self.description),
+                ('icon_url', self.icon_url),
+                ('cards', [card.to_dict() for card in self.cards])
+            ])
+        return data
     
 class Card(db.Model, SerializerMixin):
     __tablename__ = 'cards'
@@ -30,6 +68,25 @@ class Card(db.Model, SerializerMixin):
     description = db.Column(db.Text())
     rarity = db.Column(db.String())
     set_id = db.Column(db.Integer, db.ForeignKey('sets.id'), nullable=False)
+    set = db.relationship('Set', back_populates='cards')
+
+    serialize_rules = ("-cards.set", '-set')
 
     def __repr__(self):
         return f'<Card {self.name}>'
+    
+    def to_dict(self):
+        return OrderedDict([
+            ('id', self.id),
+            ('name', self.name),
+            ('description', self.description),
+            ('image_url', self.image_url),
+            ('element', self.element),
+            ('rune_type', self.rune_type),
+            ('subclass1', self.subclass1),
+            ('subclass2', self.subclass2),
+            ('attack_defense', self.attack_defense),
+            ('set_id', self.set_id),
+            ('position', self.position),
+            ('rarity', self.rarity)
+        ])
