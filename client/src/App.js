4a4c6ac9933e2b887d6191d0a32bcc663f038cf7
Evan <eroberts1998@icloud.com>
2023-06-19 21:08:37 -0400

    navbar with placeholders

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import './stylesheets/styles.css';
+import Home from './components/pages/Home.js'
+import SetsPage from './components/pages/SetsPage.js'
+import SetDetailsPage from './components/pages/SetDetailsPage.js'
+import AllCardsPage from './components/pages/AllCardsPage.js'
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
 
 function App() {
 
@@ -8,8 +15,15 @@ function App() {
   const [darkMode, setDarkMode] = useState(false)
 
   return (
-    <div className="App">
-
+    <div className={ `App ${darkMode ? 'DarkMode' : 'LightMode'}`}>
+      <Header darkMode={darkMode} updateDarkMode={() => setDarkMode((prev) => !prev)}/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sets' element={<SetsPage />} />
+        <Route path='/cards' element={<AllCardsPage />} />
+        <Route path='/sets/:id' element={<SetDetailsPage />} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
